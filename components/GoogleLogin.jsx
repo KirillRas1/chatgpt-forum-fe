15b54c8043b6e44260d1341b78c6cde229b1fe19
Kirill <kr6512592@gmail.com>
2023-03-08 00:14:53 +0200

    Add username

--- components/GoogleLogin.jsx
+++ components/GoogleLogin.jsx
@@ -1,18 +1,24 @@
 import { GoogleLogin } from '@react-oauth/google';
+import { useState } from 'react';
 import axios from 'axios';
 
-
 export default function GoogleLoginButton() {
+    const [username, setUserName] =useState('');
     const responseMessage = (response) => {
         console.log(response);
         axios.post('/login', {
             jwt: response.credential
+          }).then(function (response) {
+            setUserName(response.data.email);
           })
     };
     const errorMessage = (error) => {
         console.log(error);
     };
     return (
-        <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+        <div>
+            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+            <span>Logged in as {username}</span>
+        </div>
     )
 }
