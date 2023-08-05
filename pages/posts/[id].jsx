818a37492aa03fccedf22beb93f42c29bf77dbec
Kirill <kr6512592@gmail.com>
2023-08-05 12:33:54 +0300

    Add postContext

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, createContext } from 'react';
+import { useParams } from 'react-router-dom';
+import {postContext} from 'contexts/post'
 import PostDetails from 'components/posts/PostDetails';
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 import apiClient from 'infrastructure/apiClient';
 import Grid from '@mui/material/Unstable_Grid2';
+
+
 const PostPage = () => {
   const [post, setPost] = useState(null);
   const [comments, setComments] = useState([]);
   const router = useRouter()
-  
+
+
   useEffect(() => {
     if (router.isReady) {
       apiClient.get(`/posts/${router.query.id}/`)
@@ -16,7 +20,8 @@ const PostPage = () => {
         setPost(response.data)
         apiClient.get(`/comments/`).then((response) => {
           setComments(response.data)
-        })})
+        })
+      })
       .catch((error) => console.error('Error fetching post:', error));
     };
   }, [router.isReady]);
@@ -27,10 +32,12 @@ const PostPage = () => {
 
   return (
     <Grid container spacing={2}>
-      <PostDetails post={post} comments={comments}/>
+      <postContext.Provider value={{ comments, setComments }}>
+        {PostDetails(post)}
+      </postContext.Provider>
     </Grid>
   )
-  
+
 };
 
 export default PostPage;
