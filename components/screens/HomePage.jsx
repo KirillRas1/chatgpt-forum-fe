d6325e96009e9570c950ff40dbdaf772acd3ae25
Kirill <kr6512592@gmail.com>
2023-08-08 00:47:43 +0300

    remove unused

--- components/screens/HomePage.jsx
+++ components/screens/HomePage.jsx
@@ -8,43 +8,8 @@ import Link from 'next/link'
 
 
 export default function HomePage() {
-  const [currentMessage, setCurrentMessage] = useState('');
-  const [userMessages, setUserMessages] = useState([]);
-  const [botMessages, setBotMessages] = useState([]);
-
-  const postMessage = (message) => {
-    setUserMessages([...userMessages, message]);
-    axios.post('/generate_text', {
-      prompt: message
-    })
-    .then(function (response) { 
-      if (response.status == 200) {
-        setBotMessages([...botMessages, response.data])
-      }
-        
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-    setCurrentMessage('')
-  }
-  const mergedArray = userMessages.map(function(e, i) {
-    return [e, botMessages[i]];
-  });
   return (
     <div>
-      {/* <ul>
-        {mergedArray.map(([userMessage, botMessage], index) => (
-          <div key={index}>
-            <UserMessage>{userMessage}</UserMessage>
-            <BotMessage>{botMessage}</BotMessage>
-          </div>
-        ))}
-      </ul>
-      <input type="text" value={currentMessage} onChange={e => setCurrentMessage(e.target.value)}/>
-      <button onClick={e => postMessage(currentMessage)}>Submit</button>
-       */}
-       <Messages/>
        <Link href={'/posts/'}><h3>Posts</h3></Link>
       <GoogleLoginButton/>
     </div>
