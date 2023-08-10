6675a9b816472969dcbaa9a260d16a30c73e75ea
Kirill <kr6512592@gmail.com>
2023-08-10 21:35:53 +0300

    pretty

--- components/posts/Comment.jsx
+++ components/posts/Comment.jsx
@@ -3,43 +3,56 @@ import { Grid, Typography, Checkbox, CircularProgress } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import apiClient from 'infrastructure/apiClient';
 
-
-const Comment = ({ comment={}, allowPrompt }) => {
+const Comment = ({ comment = {}, allowPrompt }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [isPrompt, setIsPrompt] = useState(comment.is_prompt);
-  const {author, text, id} = comment;
+  const { author, text, id } = comment;
   const makePrompt = () => {
     if (isPrompt || isLoading) {
-      return
+      return;
     }
-    setIsLoading(true)
-    apiClient.patch(`comments/${id}/`, {
-      is_prompt: true
-    }).then((response) => {
-      setIsPrompt(true)
-    }).finally(() => {
-      setIsLoading(false)
-    })
-  }
+    setIsLoading(true);
+    apiClient
+      .patch(`comments/${id}/`, {
+        is_prompt: true
+      })
+      .then(response => {
+        setIsPrompt(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   const checkBox = () => {
     if (isLoading) {
-      return <CircularProgress />
+      return <CircularProgress />;
     }
-    if (allowPrompt){
-      return allowPrompt && <Checkbox checked={isPrompt} disabled={isPrompt} onChange={makePrompt}/>
+    if (allowPrompt) {
+      return (
+        allowPrompt && (
+          <Checkbox
+            checked={isPrompt}
+            disabled={isPrompt}
+            onChange={makePrompt}
+          />
+        )
+      );
     }
-  }
-
+  };
   return (
-        <Grid container spacing={0.5}>
-          <Grid item xs={4}>
-            <Divider />
-            <Typography variant="caption" color={"primary.main"}>{author || "AI"}</Typography >
-            {checkBox()}
-            <Typography variant="body1" width="auto">{text}</Typography >
-          </Grid>
-        </Grid>
+    <Grid container spacing={0.5}>
+      <Grid item xs={4}>
+        <Divider />
+        <Typography variant="caption" color={'primary.main'}>
+          {author || 'AI'}
+        </Typography>
+        {checkBox()}
+        <Typography variant="body1" width="auto">
+          {text}
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
--- components/posts/PostList.jsx
+++ components/posts/PostList.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import Link from 'next/link';
 import { Typography, List, ListItem, ListItemText } from '@mui/material';
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
 
 const PostList = (posts = []) => {
-  
-  const router = useRouter()
-  const handlePostClick = (id) => () => {
-    router.push({pathname: `/posts/${id}`})
-  }
+  const router = useRouter();
+  const handlePostClick = id => () => {
+    router.push({ pathname: `/posts/${id}` });
+  };
   if (!posts) {
     return <Typography>Loading...</Typography>;
   }
   return (
     <div>
-      <Typography  variant="h2">Posts</Typography>
+      <Typography variant="h2">Posts</Typography>
       <List>
-      {posts.map((post) => (
+        {posts.map(post => (
           <ListItem key={post.id} onClick={handlePostClick(post.id)}>
             <Typography variant="h3">{post.title}</Typography>
             <Typography variant="body1">Author: {post.author}</Typography>
