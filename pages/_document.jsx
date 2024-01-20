9508d3d9ba5db7af86e994e17c7c81753cd0733e
Kirill <kr6512592@gmail.com>
2024-01-20 20:28:24 +0200

    test meta

---
+++ pages/_document.jsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+ 
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <meta name="google-adsense-account" content="ca-pub-9479755495142783"></meta>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
