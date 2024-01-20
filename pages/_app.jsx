284e76b4fe77d4ef2dc516bdfe2954f974d6a937
Kirill <kr6512592@gmail.com>
2024-01-20 19:32:57 +0200

    try with tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,16 @@
 import DefaultLayout from 'layouts/DefaultLayout';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from 'contexts/Auth';
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+      // _app.js
+<Script
+  async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9479755495142783"
+  crossorigin="anonymous"
+/>
       <AuthProvider>
         <DefaultLayout>
           <Component {...pageProps} />
