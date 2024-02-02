ceffb59cbaaf8429686279ec4d294bc9f0b06c72
Kirill <kr6512592@gmail.com>
2024-02-02 22:06:44 +0200

    Add title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,9 +2,15 @@ import DefaultLayout from 'layouts/DefaultLayout';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from 'contexts/Auth';
 import Script from 'next/script';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+    <title>Geppeta Board</title>
+      </Head>
+      
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
 <Script
   async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9479755495142783"
@@ -16,5 +22,6 @@ export default function MyApp({ Component, pageProps }) {
         </DefaultLayout>
       </AuthProvider>
     </GoogleOAuthProvider>
+    </>
   );
 }
