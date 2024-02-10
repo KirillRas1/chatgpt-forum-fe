14d6380c0e1b9704656fc178524588dbe9a5f496
Kirill <kr6512592@gmail.com>
2024-02-10 23:57:48 +0200

    Add meta tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,16 +7,6 @@ import Head from 'next/head';
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-    <title>Geppeta Board</title>
-    <link rel="preconnect" href="https://kirillras.net" crossOrigin="true"/>
-    <link rel="preconnect" href="https://adservice.google.com"/>
-    <link rel="preconnect" href="https://googleads.g.doubleclick.net"/>
-    <link rel="preconnect" href="https://www.googletagservices.com"/>
-    <link rel="preconnect" href="https://tpc.googlesyndication.com"/>
-    <link rel="preconnect" href="https://pagead2.googlesyndication.com"/>
-    <link rel="preconnect" href="https://accounts.google.com"/>
-    </Head>
     <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
 <Script
   async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9479755495142783"
