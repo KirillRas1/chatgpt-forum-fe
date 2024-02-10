b303bd5bbe134fd05a52a7763f32cd0e40810c83
Kirill <kr6512592@gmail.com>
2024-02-11 00:01:14 +0200

    fix

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,3 @@
 export default function Custom404() {
-    return <h1>404 - Page Not Found, Click on "Home Page" to go back</h1>
+    return <h1>404 - Page Not Found, Click on {"Home Page"} to go back</h1>
   }
