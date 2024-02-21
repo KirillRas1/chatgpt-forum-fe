ad67bda305e813b233854eea09ad504c05f693a4
Kirill <kr6512592@gmail.com>
2024-02-21 23:32:07 +0200

    Migrated document

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import DefaultLayoutClientSide from 'layouts/DefaultLayout';
 
 export const metadata = {
+  title: 'Geppeta Board',
   description:
     'Engage in collaborative conversations with ChatGPT at our forum, where discussions organically branch out in a tree-like structure.',
   'google-adsense-account': 'ca-pub-9479755495142783'
@@ -12,6 +13,20 @@ export const metadata = {
 export default function DefaultLayoutServerSide({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="preconnect"
+          href="https://kirillras.net"
+          crossOrigin="true"
+        />
+        <link rel="preconnect" href="https://adservice.google.com" />
+        <link rel="preconnect" href="https://googleads.g.doubleclick.net" />
+        <link rel="preconnect" href="https://www.googletagservices.com" />
+        <link rel="preconnect" href="https://tpc.googlesyndication.com" />
+        <link rel="preconnect" href="https://pagead2.googlesyndication.com" />
+        <link rel="preconnect" href="https://accounts.google.com" />
+        <meta name="google-adsense-account" content="ca-pub-9479755495142783" />
+      </head>
       <body>
         <GlobalContextProvider>
           <Script
