ece8e83ddd5099cf0d91bc63694c0bd49d6ad23e
Kirill <kr6512592@gmail.com>
2024-03-02 15:15:48 +0200

    deploy

--- components/GoogleLogin.jsx
+++ components/GoogleLogin.jsx
@@ -1,17 +1,35 @@
 'use client';
-import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import {
+  GoogleLogin,
+  googleLogout,
+  useGoogleOneTapLogin,
+  useGoogleLogin
+} from '@react-oauth/google';
 import React, { useContext, useState } from 'react';
 import { Box, Button } from '@mui/material';
 import { authContext } from 'contexts/Auth';
 import SignUpButton from 'components/auth/Signup';
 import LoginModal from './auth/Login';
-
+import ReactDOM from 'react-dom';
+import FacebookLogin from 'react-facebook-login';
+import apiClient from 'infrastructure/api/apiClient';
 export default function GoogleLoginButton() {
   const { login, logout, displayName } = useContext(authContext);
 
   const responseMessage = responseFromGoogle => {
+    console.log(responseFromGoogle);
     login(responseFromGoogle.credential);
+    apiClient.post('auth/google/', {
+      access_token: responseFromGoogle.credential
+    });
   };
+
+  const authCodeLogin = useGoogleLogin({
+    onSuccess: async codeResponse => {
+      await apiClient.post('auth/google/', { code: codeResponse.code });
+    },
+    flow: 'auth-code'
+  });
 
   return (
     <div>
@@ -19,6 +37,13 @@ export default function GoogleLoginButton() {
         <Box display="flex" flexDirection="row" gap="5px" alignItems="center">
           <LoginModal />
           <SignUpButton />
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => authCodeLogin()}
+          >
+            Google
+          </Button>
           <GoogleLogin onSuccess={responseMessage} />
         </Box>
       ) : (
