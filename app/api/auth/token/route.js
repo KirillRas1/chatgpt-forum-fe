edd168c897482228b307d5f050741822f97db727
Kirill <kr6512592@gmail.com>
2024-12-14 22:51:46 +0200

    return error

--- app/api/auth/token/route.js
+++ app/api/auth/token/route.js
@@ -2,6 +2,11 @@ import { NextResponse } from 'next/server';
 import { getAccessToken } from '@auth0/nextjs-auth0';
 
 export async function GET() {
-  const { accessToken } = await getAccessToken();
-  return NextResponse.json({ token: accessToken });
+  try {
+    const { accessToken } = await getAccessToken();
+    return Response.json({ token: accessToken });
+  } catch (e) {
+    return Response.json({ error: e }, { status: 500 });
+  } 
+  
 }
