040b9b58cf7f36de3f2a978d032f302e16d0c9fc
Kirill <kr6512592@gmail.com>
2024-12-19 23:06:15 +0200

    fix logout CORS

--- components/buttons/Login.js
+++ components/buttons/Login.js
@@ -2,16 +2,10 @@
 'use client';
 import React from 'react';
 import Button from '@mui/material/Button';
-import { useRouter } from 'next/navigation';
 const LoginButton = () => {
-  const router = useRouter();
-  const handleLogin = () => {
-    router.push('/api/auth/login');
-  };
-
   return (
     <a href="/api/auth/login">
-      <Button variant="contained" color="secondary" >
+      <Button variant="contained" color="secondary">
         Login
       </Button>
     </a>
--- components/buttons/Logout.js
+++ components/buttons/Logout.js
@@ -1,17 +1,13 @@
 'use client';
 import React from 'react';
 import Button from '@mui/material/Button';
-import { useRouter } from 'next/navigation';
 const LogoutButton = () => {
-  const router = useRouter();
-  const handleLogout = () => {
-    router.push('/api/auth/logout');
-  };
-
   return (
-    <Button variant="contained" color="secondary" onClick={handleLogout}>
-      Logout
-    </Button>
+    <a href="/api/auth/logout">
+      <Button variant="contained" color="secondary" onClick={handleLogout}>
+        Logout
+      </Button>
+    </a>
   );
 };
 
